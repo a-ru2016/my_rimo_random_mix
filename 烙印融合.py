bcba1f46cc00aadc27ec0033c7e47863e3293464
a-ru2016 <124528449+a-ru2016@users.noreply.github.com>
2024-03-22 15:09:23 +0900

    Add files via upload

--- 烙印融合.py
+++ 烙印融合.py
@@ -9,6 +9,8 @@ from bayes_opt import BayesianOptimization
 from safetensors.numpy import load_file, save_file
 import glob
 import argparse
+from huggingface_hub import HfApi
+from pathlib import Path
 
 sys.path.append(os.path.join(os.path.dirname(__file__), './stable-diffusion-anime-tag-benchmark'))
 from common import 上网, 服务器地址
@@ -23,7 +25,7 @@ def setup_parser() -> argparse.ArgumentParser:
     parser.add_argument(
         '--amp',
         default="float16",
-        help='計算精度[float16,float32]')
+        help='計算精度[float16,float32,int8]')
     return parser
 
 parser = setup_parser()
@@ -31,6 +33,8 @@ args = parser.parse_args()
 
 if args.amp=="float16":
     amp = np.float16
+elif args.amp=="int8":
+    amp = np.int8
 else:
     amp = np.float32
 
@@ -65,6 +69,24 @@ def 融合识别(s: str) -> str:
             return f'{k}_{n}'
     return 'r'
 
+def upload_file(file_path, token):
+          file_name = "モデル/"+Path(file_path).name
+          print(f"Uploading {file_name} to https://huggingface.co/datasets/{datasets_repo}")
+          print(f"Please wait...")
+
+          api.upload_file(
+              path_or_fileobj=file_path,
+              path_in_repo=file_name,
+              repo_id=datasets_repo,
+              repo_type="dataset",
+              commit_message=commit_message,
+              token=token,
+          )
+          print(f"Upload success, located at https://huggingface.co/datasets/{datasets_repo}/blob/main/{file_name}\n")
+api = HfApi()
+datasets_repo = "aru2016/model"
+commit_message = "烙印融合.py"
+auth_token = "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"
 
 def 名字(kw: dict):
     s = sorted(kw.items())
@@ -80,8 +102,10 @@ def 烙(**kw):
         weighted_sum = 0
         for i, mdl in enumerate(model):
             weighted_sum += mdl[k] * kw[f'{qk}_{i}']
-        新模型[k] = weighted_sum
-    save_file(新模型, f'{模型文件夹}/{文件名}.safetensors')
+        新模型[k] = weighted_sum.astype(np.float16)
+    file_path = f'{模型文件夹}/{文件名}.safetensors'
+    save_file(新模型, file_path)
+    upload_file(file_path,auth_token)
     del 新模型
     上网(f'{服务器地址}/sdapi/v1/refresh-checkpoints', method='post')
     结果 = 评测模型(文件名, 'sdxl_vae_fp16fix.safetensors', 32, n_iter=80, use_tqdm=False, savedata=true, seed=22987, tags_seed=2223456, 计算相似度=False)
@@ -97,6 +121,7 @@ def 烙(**kw):
     print(文件名, acc, mm.shape)
     with open(记录文件名, 'w', encoding='utf8') as f:
         json.dump(记录, f, indent=2)
+    os.remove(file_path)
     return acc
 
 识别结果 = set([融合识别(k) for k in all_k])
@@ -107,7 +132,7 @@ for i, mdl in enumerate(model):
 optimizer = BayesianOptimization(
     f=烙,
     pbounds={k: (-1, 1) for k in all_params},
-    random_state=1,
+    random_state=777,
     #verbose=2.
 )
 optimizer.probe(params={k: 0 for k in all_params})
